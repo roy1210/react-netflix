d11b41f25892065346f2bba47fbbefcd1b48fd25
”roy1210” <rswkure@gmail.com>
2019-11-30 14:08:31 +0800

    Styled footer section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,24 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { Icon } from "react-icons-kit";
+import { iosWorld } from "react-icons-kit/ionicons/iosWorld";
+import { arrowSortedDown } from "react-icons-kit/typicons/arrowSortedDown";
 
 class Footer extends Component {
+  state = {
+    langContent: false
+  };
+  handleToggle = e => {
+    e.preventDefault();
+    this.setState({
+      langContent: !this.state.langContent
+    });
+  };
   render() {
     return (
       <FooterContainer>
-        <span>
+        <span style={{ marginLeft: "15%", fontSize: "1.1rem" }}>
           Questions? Call <Link>1-872-742-1335</Link>
         </span>
         <div className="footer-columns">
@@ -72,7 +84,28 @@ class Footer extends Component {
               <Link>Legal Notices</Link>
             </li>
           </ul>
+          {/* Language Button */}
+          <div className="lang-btn" onClick={this.handleToggle}>
+            <Icon icon={iosWorld} size={20} />
+            &nbsp;&nbsp;English&nbsp;&nbsp;
+            <Icon icon={arrowSortedDown} />
+          </div>
         </div>
+
+        {/* Loggle Language content */}
+        {this.state.langContent && (
+          <div className="lang-toggle">
+            <ul>
+              <li>English</li>
+            </ul>
+            <ul>
+              <li>French</li>
+            </ul>
+          </div>
+        )}
+        <span style={{ marginLeft: "15.5%", fontSize: "0.9rem" }}>
+          Netflix Singapore
+        </span>
       </FooterContainer>
     );
   }
@@ -93,5 +126,44 @@ const FooterContainer = styled.footer`
     overflow: auto;
     display: grid;
     grid-template-columns: repeat(4, 1fr);
+  }
+
+  ul li {
+    list-style: none;
+    line-height: 2.5;
+  }
+
+  a {
+    color: #999;
+  }
+  a:hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+  .lang-btn {
+    background: transparent;
+    border: 0.9px solid #333;
+    padding: 1rem;
+    width: 8rem;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    margin: 2rem 0 2rem;
+    cursor: pointer;
+  }
+
+  .lang-toggle {
+    margin-left: 15%;
+    position: absolute;
+    margin-top: -2rem;
+  }
+  .lang-toggle ul {
+    background: var(--main-deep-dark);
+    width: 8.125rem;
+    border: 1px solid #333;
+    text-align: center;
+    &:hover {
+      background: #0085ff;
+      color: #fff;
+    }
   }
 `;
